6e6d8240cea50e40ca785c5e078436c5fd04ee65
BugHunter-X <askzenai@gmail.com>
2024-12-29 03:52:04 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,7 @@
+function myFunc() {
+  if (isset($myArray[0])) {
+    // ... further code using $myArray[0] ...
+  } else {
+    return null; // Or handle the empty array case appropriately
+  }
+} 
